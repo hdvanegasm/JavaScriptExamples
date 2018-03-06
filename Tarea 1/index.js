dd5796050bafeb980708dcf702f1847646b62917
hernan232 <hdvanegasm@gmail.com>
2018-03-06 14:03:13 -0500

    Add new input

--- Tarea 1/index.js
+++ Tarea 1/index.js
@@ -1,12 +1,17 @@
 function sendData() {
-  var textboxValue = document.getElementById('username').value;
-  var result = httpGet('http://httpbin.org/post', textboxValue);
-  console.log(JSON.parse(result).data); 
+  var usernameInputValue = document.getElementById('firstname').value;
+  var lastnameInputValue = document.getElementById('lastname').value;
+  var sendData = {
+    firstname: usernameInputValue,
+    lastname: lastnameInputValue
+  };
+  var result = httpGet('http://httpbin.org/post', JSON.stringify(sendData));
+  console.log(JSON.parse(result).data);
 }
 
-function httpGet(url, value) {
+function httpGet(url, data) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open('POST', url, false);
-  xmlHttp.send(value);
+  xmlHttp.send(data);
   return xmlHttp.response;
 }
